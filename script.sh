fb021e7210c443188963eeeabcf99a049e990751
HexagonPrime <8053335677@qq.com>
2021-04-21 11:29:53 +0100

    update

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --constrain='titan_xp|geforce_gtx_titan_x'
 
-source /scratch/shecai/conda/etc/profile.d/conda.sh
+source /scratch_net/biwidl306/shecai/conda/etc/profile.d/conda.sh
 conda activate pixelnerf
 #module load libs/cuda
-python train/train.py -n dtu_exp -c conf/exp/dtu.conf -D /scratch-second/shecai/rs_dtu_4 -V 3 --gpu_id=0
+python train/train.py -n dtu_exp -c conf/exp/dtu.conf -D /scratch_net/biwidl306/shecai/rs_dtu_4 -V 3 --gpu_id=0
