0e11dd5222c3e9232cb105e82cd3fc517124f1b9
HexagonPrime <8053335677@qq.com>
2021-04-21 14:27:30 +0100

    update

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 #SBATCH --output=log/dtu.out
 #SBATCH --mem=50G
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --constrain='geforce_rtx_2080_ti|geforce_gtx_1080_ti|titan_xp|geforce_gtx_titan_x'
 
 source /scratch_net/biwidl306/shecai/conda/etc/profile.d/conda.sh
 conda activate pixelnerf
 #module load libs/cuda
-python train/train.py -n dtu_exp -c conf/exp/dtu.conf -D /scratch_net/biwidl306/shecai/rs_dtu_4 -V 3 --gpu_id=0 -R 2000
+python train/train.py -n dtu_exp -c conf/exp/dtu.conf -D /scratch_net/biwidl306/shecai/rs_dtu_4 -V 3 --gpu_id='0 1' -R 2000
